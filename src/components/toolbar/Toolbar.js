a4a2c6908d87ba48d5045ba1900c22e6f9131333
Dima <hodarenokd@gmail.com>
2022-07-12 13:05:44 +0300

    remove event listeners

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,6 +3,13 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 export class Toolbar extends ExcelComponent {
     static className = 'excel__toolbar';
 
+    constructor($root) {
+      super($root, {
+        name: 'Toolbar',
+        listeners: ['click']
+      })
+    }
+
     toHTML() {
         return `
         <div class="button">
@@ -24,5 +31,9 @@ export class Toolbar extends ExcelComponent {
             <span class="material-symbols-outlined"> format_underlined </span>
           </div>
         `
+    }
+
+    onClick(event) {
+      console.log(event.target);
     }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -16,12 +16,20 @@ export class DomListener {
             if (!this[method]) {
                 throw new Error(`Methid ${method} is not implemented in ${this.name} Component`)
             }
-            this.$root.on(listener, this[method].bind(this));
+            this[method] = this[method].bind(this);
+            console.log('this[method]: ', this[method]);
+            this.$root.on(listener, this[method]);
         })
     }
 
     removeDOMListeners() {
-
+        this.listeners.forEach((listener) => {
+            const method = getMethodName(listener);
+            if (!this[method]) {
+                throw new Error(`Methid ${method} is not implemented in ${this.name} Component`)
+            }
+            this.$root.off(listener, this[method]);
+        })
     }
 }
 
